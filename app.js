9ef41b44f26f7db0423c65796a4d5cb4c3008f85
Thirumalai Aiyalu <taiyalu@taiyalu-macbookpro2.roam.corp.google.com>
2018-07-05 17:19:04 +0800

    seriously?

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ var mime = require('mime');
 var multer = require('multer');
 var upload = multer({dest: 'uploads/'});
 var os = require('os');
-var base64 = require('node-base64-image');
 
 // Set up auth
 var vision = require('@google-cloud/vision');
@@ -63,13 +62,11 @@ app.get('/', function(req, res) {
 // Get the uploaded image
 // Image is uploaded to req.file.path
 app.post('/upload', upload.single('image'), function(req, res, next) {
+  
 
   // Choose what the Vision API should detect
   // Choices are: faces, landmarks, labels, logos, properties, safeSearch, texts
   var types = ['labels'];
-  var request = {
-      image: {source: {filename: req.file.path} },
-  }
   // Send the image to the Cloud Vision API
   vision1.labelDetection(request, function(err, detections) {
     if (err) {
@@ -79,10 +76,8 @@ app.post('/upload', upload.single('image'), function(req, res, next) {
         'Content-Type': 'text/html'
       });
       res.write('<!DOCTYPE HTML><html><body>');
-      var src = base64.encode(req.file.path);
-
       // Base64 the image so we can display it on the page
-      res.write('<img width=200 src="' + src + '"><br>');
+      res.write('<img width=200 src="' + base64Image(req.file.path) + '"><br>');
 
       // Write out the JSON output of the Vision API
       res.write(JSON.stringify(detections, null, 4));
@@ -100,8 +95,8 @@ console.log('Server Started on port 8080');
 
 // Turn image into Base64 so we can display it easily
 
-// function base64Image(src) {
-//   var data = fs.readFileSync(src).toString('base64');
-//   return util.format('data:%s;base64,%s', mime.lookup(src), data);
-// }
+function base64Image(src) {
+  var data = fs.readFileSync(src).toString('base64');
+  return util.format('data:%s;base64,%s', mime.getType(src), data);
+}
 
